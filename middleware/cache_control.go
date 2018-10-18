7749a3312b5bc34e15899db51b8b92d50c9bc221
Luis G <luis.gg1990@gmail.com>
2018-10-18 12:10:01 -0300

    Fix

--- middleware/cache_control.go
+++ middleware/cache_control.go
@@ -3,24 +3,17 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/lggomez/cachecontrol-lite/middleware/cacheobject"
-	"strconv"
 )
 
 const (
 	CacheControlHeader = "Cache-Control"
-	StatusCodeHeader   = "Status-Code"
 )
 
 // WithCacheControl is a wrapper middleware for adding Cache-Control headers
 func AddCacheControl(directives *cacheobject.ResponseCacheDirectives) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Validate response state and build header
-		status := 0
-		statusHeader := c.GetHeader(StatusCodeHeader)
-		if statusHeader != "" {
-			status, _ = strconv.Atoi(statusHeader)
-		}
-		// Only cache valid 2xx requests
+		status := c.Writer.Status()
+		// Emit header only for valid 2xx requests
 		if !c.IsAborted() && (status >= 200 && status <= 299) {
 			c.Header(CacheControlHeader, directives.BuildResponseHeader())
 		}
