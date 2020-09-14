d30aac8c7383ce15b8f8304081ab018250527df9
Luis Gabriel Gomez <luis.gg1990@gmail.com>
2020-09-14 12:25:58 -0300

    Refactor packages, add go.mod

---
+++ middleware/common.go
@@ -0,0 +1,6 @@
+package middleware
+
+const (
+	CacheControlHeader = "Cache-Control"
+)
+
--- middleware/gin/cache_control.go
+++ middleware/gin/cache_control.go
@@ -1,21 +1,18 @@
-package middleware
+package gin
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lggomez/cachecontrol-lite/middleware"
 	"github.com/lggomez/cachecontrol-lite/middleware/cacheobject"
 )
 
-const (
-	CacheControlHeader = "Cache-Control"
-)
-
 // WithCacheControl is a wrapper middleware for adding Cache-Control headers
-func AddCacheControl(directives *cacheobject.ResponseCacheDirectives) gin.HandlerFunc {
+func WithCacheControl(directives *cacheobject.ResponseCacheDirectives) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		status := c.Writer.Status()
 		// Emit header only for valid 2xx requests
 		if !c.IsAborted() && (status >= 200 && status <= 299) {
-			c.Header(CacheControlHeader, directives.BuildResponseHeader())
+			c.Header(middleware.CacheControlHeader, directives.BuildResponseHeader())
 		}
 	}
 }
